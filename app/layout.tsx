c3af2921accdb0ad5f60f006c84a6bcd17d0a83d
Hammad Afzal <hammad.afzal@pyflowlabs.com>
2026-07-14 16:20:48 +0500

    feat: added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Manrope } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Analytics } from '@vercel/analytics/next';
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -57,6 +58,7 @@ export default function RootLayout({
         <Header />
         <main id="main-content" className="w-full overflow-x-hidden">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
